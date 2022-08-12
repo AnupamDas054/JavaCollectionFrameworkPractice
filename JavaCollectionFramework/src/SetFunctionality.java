d37383ff1637506a9cf681dfc8916c70ef81f29e
anupam <anupamdas0376@gmail.com>
2022-08-13 00:51:56 +0600

    Add LinkedHashSet functionality

--- JavaCollectionFramework/src/SetFunctionality.java
+++ JavaCollectionFramework/src/SetFunctionality.java
@@ -1,11 +1,13 @@
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Set;
 
 public class SetFunctionality {
 
     public  static  void main(String[] args)
     {
-        Set<Integer> set = new HashSet<>();
+        Set<Integer> set = new HashSet<>();// order is not maintained
+       // Set<Integer> set = new LinkedHashSet<>();//order is maintained according to insert
         set.add(5);
         set.add(1);
         set.add(34);
